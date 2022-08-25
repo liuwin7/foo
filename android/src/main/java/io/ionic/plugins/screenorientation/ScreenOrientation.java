3d0a06943849c4eab839b8fc28833f236c05461f
liuwin7 <liuwin7@163.com>
2022-08-25 15:24:39 +0800

    Initial commit

---
+++ android/src/main/java/io/ionic/plugins/screenorientation/ScreenOrientation.java
@@ -0,0 +1,11 @@
+package io.ionic.plugins.screenorientation;
+
+import android.util.Log;
+
+public class ScreenOrientation {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
